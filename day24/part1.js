01a1c21dc3ac1ce21550020a7a8a679ec1913f99
Jake Helme <jake.helme@gmail.com>
2021-12-26 16:54:20 +0200

    day 24

---
+++ day24/part1.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+const rawInput = fs.readFileSync("./day24/input.txt").toString();
+
+const instructions = rawInput.split("\n").map((inst) => inst.split(" "));
+const A = instructions
+  .filter((_, i) => i % 18 === 4)
+  .map((x) => x[2])
+  .map(Number);
+const B = instructions
+  .filter((_, i) => i % 18 === 5)
+  .map((x) => x[2])
+  .map(Number);
+const C = instructions
+  .filter((_, i) => i % 18 === 15)
+  .map((x) => x[2])
+  .map(Number);
+
+const stack = [];
+const monad = new Array(14);
+  for (let i = 0; i < 14; i++) {
+  if (A[i] === 1) {
+    stack.push({index: i, val: C[i]});
+  } else {
+    const popped = stack.pop();
+    const adjustor = popped.val + B[i];
+    if(adjustor < 0) {
+      monad[popped.index] = 9;
+      monad[i] = 9 + adjustor;
+    } else {
+      monad[popped.index] = 9 - adjustor;
+      monad[i] = 9;
+    }
+  }
+}
+
+console.log(monad.join(''));
